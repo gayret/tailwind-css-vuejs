f1d3bd8d9ff3d0c2638ad8a1dfabee4a28770c90
Safa Gayret <safa.gayret@tsoft.com.tr>
2022-05-29 14:14:44 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { reactive } from '@vue/reactivity'
+
+const state = reactive({
+  count: 0,
+})
+
+const increment = () => {
+  state.count++
+}
+
+const decrement = () => {
+  state.count--
+}
+</script>
+
+<template>
+  <div class="grid center gap-1 p-4">
+    <h1 class="text-center text-3xl">{{ state.count }}</h1>
+    <div class="flex justify-center gap-2">
+      <button class="btn-green" @click="increment">+ Increment</button>
+      <button class="btn-gray" @click="decrement">- Decrement</button>
+    </div>
+  </div>
+</template>
