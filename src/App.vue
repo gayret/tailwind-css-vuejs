39ffe7cdb77e2a37d3e7723b28fa07b3815628f9
Safa Gayret <safa.gayret@tsoft.com.tr>
2022-06-05 14:30:30 +0300

    copilot video recorded

--- src/App.vue
+++ src/App.vue
@@ -18,8 +18,8 @@ const decrement = () => {
   <div class="grid center gap-1 p-4">
     <h1 class="text-center text-3xl">{{ state.count }}</h1>
     <div class="flex justify-center gap-2">
-      <button class="btn-green" @click="increment">+ Increment</button>
-      <button class="btn-gray" @click="decrement">- Decrement</button>
+      <button class="btn-primary" @click="increment">+ Increment</button>
+      <button class="btn-danger" @click="decrement">- Decrement</button>
     </div>
   </div>
 </template>
